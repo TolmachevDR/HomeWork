e7ea017b1ccc4560e8e1a91ada65cecc66b066b1
Tolmachev Denis <reinor1995@gmail.com>
2022-08-13 17:20:46 +0900

    selfwork

---
+++ seminar3/self/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 18: Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+
+
+Console.Clear();
+Console.WriteLine("Введите номер плоскости для определения диапазона координат точек: ");
+
+void PrintplateNumber(int plateNumber)
+{
+string range = "Error";
+if (plateNumber == 1)
+    Console.Write("Ось x, ось y"); 
+if (plateNumber == 2)
+    Console.Write("Ось -x, ось y"); 
+if (plateNumber == 3)
+    Console.Write("Ось x, ось -y");
+if (plateNumber == 4)
+    Console.Write("Ось -x, ось -y");
+}
+
+int plateNumber = int.Parse(Console.ReadLine()!);
+Print.plateNumber(plateNumber);
