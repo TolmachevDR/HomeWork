0aeb2d6d966ebbec841ab924bff9a1b5a4ecd57c
Tolmachev Denis <reinor1995@gmail.com>
2022-08-14 19:37:08 +0900

    done seminar4 self and self2

---
+++ seminar4/self/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+
+Console.Clear();
+Console.Write("Введите число: ");
+int anyNumber = Convert.ToInt32(Console.ReadLine()!);
+string numberText = Convert.ToString(anyNumber);
+int count = 0;
+while (anyNumber > 0)
+{
+    count++;
+    anyNumber /= 10;
+}
+Console.WriteLine("Количество цифр в вашем числе " + count);
+Console.WriteLine("Количество цифр в вашем числе " + numberText.Length);
---
+++ seminar4/self2/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24 
+// 5 -> 120
+
+// Задача 30: Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+// int size = 8;
+// int[] array = new int[size]; arr = ([0, 1] * n);
+
+int[] RandomNumbersArray(int size)
+{
+    int[] numbersArray = new int[size];
+    var rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        numbersArray[i] = rnd.Next(2);
+    } 
+    return numbersArray;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+int size = 8;
+int[] array = RandomNumbersArray(size);
+PrintArray(array);
