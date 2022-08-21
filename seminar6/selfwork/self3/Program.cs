41dc23566d1b058c6be83bd66eb2ba05fd44fcaa
Tolmachev Denis <reinor1995@gmail.com>
2022-08-21 17:20:53 +0900

    done seminar6

---
+++ seminar6/selfwork/self3/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+
+class Convert
+{
+    static void Main()
+    {
+        int num10 = 2; // число в десятичной системе исчисления
+        int a = 0;  // a - остаток после % деления из которого формируется число в 
+                    //    двоичной системе исчисления
+        int i = 0;
+        int[] b = new int[10]; // массив с помощью которого двоичное число позже 
+                                 //будет выведено с конца для правильного отображения 
+ 
+        while (num10 >= 1)
+        {
+            a = num10 % 2;
+            b[i] = a;
+            i++;
+            num10 = num10 / 2;
+            Console.Write(a);
+        };
+    }
+ 
+}
+
+
+string check(int n)
+{
+    string? result = "";
+    while (n > 0)
+    {
+        int x = n % 2;
+        result = Convert.ToString(x) + result;
+        n /= 2;
+    }
+    return result;
+
+}
+Console.Write("Введите число, которое необходимо перевести в 2-ую систему: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(check(n));
---
+++ seminar6/selfwork/self5/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 45: Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования.
+
+void NewArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(-10, 11);
+}
+
+int[] CreateArray(int[] array)
+{
+    int[] copyArray = new int[array.Length];
+    for (int i = 0; i < array.Length; i++)
+        copyArray[i] = array[i];
+    
+    return copyArray;
+}
+
+Console.Write("Укажите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+int[] copyArray = new int[n];
+NewArray(array);
+Console.WriteLine("Исходный массива [" + string.Join(", ", array) + "]");
+copyArray = CreateArray(array);
+copyArray[0] = 20;
+Console.WriteLine("Исходный массива [" + string.Join(", ", copyArray) + "]");
